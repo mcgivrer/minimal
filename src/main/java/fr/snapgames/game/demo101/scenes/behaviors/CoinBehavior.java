f2c21d649872251de7a7cf272f39ddb0646699e4
Frédéric Delorme <frederic.delorme@gmail.com>
2023-02-08 18:46:09 +0100

    feat(i18n): Add French translation and fix bugs

--- src/main/java/fr/snapgames/game/demo101/scenes/behaviors/CoinBehavior.java
+++ src/main/java/fr/snapgames/game/demo101/scenes/behaviors/CoinBehavior.java
@@ -18,6 +18,8 @@ public class CoinBehavior implements Behavior<GameEntity> {
     @Override
     public void draw(Game game, Graphics2D g, GameEntity e) {
         if (game.getDebug() > 1) {
+            Stroke bckUp = g.getStroke();
+            setDashLine(g);
             double attrDist = (double) e.getAttribute("attractionDistance", 0);
             if (attrDist > 0) {
                 g.setColor(Color.YELLOW);
@@ -26,7 +28,20 @@ public class CoinBehavior implements Behavior<GameEntity> {
                         e.size.x + (attrDist * 2.0), e.size.y + (attrDist * 2.0));
                 g.draw(el);
             }
+            g.setStroke(bckUp);
         }
+    }
+
+    public void setDashLine(Graphics2D g) {
+        float[] dash1 = {2f, 0f, 2f};
+
+        BasicStroke bs1 = new BasicStroke(1,
+                BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_ROUND,
+                1.0f,
+                dash1,
+                2f);
+        g.setStroke(bs1);
     }
 
     @Override
@@ -38,11 +53,12 @@ public class CoinBehavior implements Behavior<GameEntity> {
             double attrDist = (double) entity.attributes.get("attractionDistance");
             double attrForce = (double) entity.attributes.get("attractionForce");
 
-            if (p.position.add(entity.size.multiply(0.5)).distance(entity.position.add(p.size.multiply(0.5))) < attrDist) {
+            if (p.position.add(entity.size).distance(entity.position.add(p.size.multiply(0.5))) < attrDist) {
                 Vector2D v = p.position.substract(entity.position);
                 entity.forces.add(v.normalize().multiply(attrForce));
             }
-            if (p.position.add(entity.size.multiply(0.5)).distance(entity.position.add(p.size.multiply(0.25))) < entity.size.add(p.size)
+            if (p.position.add(entity.size.multiply(0.50)).distance(entity.position.add(p.size.multiply(0.50)))
+                    < entity.size.add(p.size)
                     .multiply(0.25).length()) {
                 entity.setActive(false);
                 int score = (int) p.getAttribute("score", 0);
